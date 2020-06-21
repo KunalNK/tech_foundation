12d5a1fd5df00c7dd6bc379ed404090fa2e137f2
Kunal <knkillekar@gmail.com>
2020-06-21 18:51:39 +0530

    four functions for calculator

---
+++ calc.py
@@ -0,0 +1,17 @@
+#Function to add two numbers  
+def add(num1, num2):
+    return num1 + num2
+
+#Function to substract two numbers 
+def sub(num1,num2):
+    return num1-num2
+
+#Function to multiply two numbers
+def mul(num1,num2):
+    return num1*num2
+
+#Function to divide two numbers
+def div(num1,num2):
+    return num1/num2
+
+
