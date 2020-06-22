8084a2ff34c830b4451ea6ceac851887b41b48fe
Kunal <knkillekar@gmail.com>
2020-06-22 19:30:17 +0530

    Updated calc code

--- calc.py
+++ calc.py
@@ -1,18 +1,18 @@
 #Function to add two numbers  
-def add(num1, num2):
-    return num1 + num2
+def add(num1, num2,num3,num4):
+    return num1 + num2 + num3 + num4
 
 #Function to substract two numbers 
-def sub(num1,num2):
-    return num1-num2
+def sub(num1,num2, num3, num4):
+    return num1-num2-num3-num4
 
 #Function to multiply two numbers
-def mul(num1,num2):
-    return num1*num2
+def mul(num1,num2,num3,num4):
+    return num1*num2*num3*num4
 
 #Function to divide two numbers
-def div(num1,num2):
-    return num1/num2
+def div(num1,num2,num3,num4):
+    return num1/num2/num3/num4
 
 print("Please select the option -\n"\
         "1. Add\n" \
@@ -25,24 +25,27 @@ print("Please select the option -\n"\
 # Take input from the user  
 select = int(input("Select operations form 1, 2, 3, 4 :"))
 
-number_1 = int(input("Enter first number: ")) 
-number_2 = int(input("Enter second number: ")) 
+number_1 = int(input("Enter first number: ") or "8") 
+number_2 = int(input("Enter second number: ") or "4")
+number_3 = int(input("Enter third number: ") or "2") 
+number_4 = int(input("Enter fourth number: ") or "1")  
 
 if select==1:
-    print(number_1, "+", number_2, "=",
-            add(number_1, number_2))
+    print(number_1, "+", number_2, "+", number_3, "+", number_4, "=",
+            add(number_1, number_2,number_3,number_4))
+
 
 elif select==2:
-    print(number_1, "-", number_2, "=",
-            sub(number_1, number_2))
+    print(number_1, "-", number_2, "-", number_3, "-", number_4, "=",
+            sub(number_1, number_2, number_3, number_4))
 
 elif select==3:
-    print(number_1, "*", number_2, "=",
-            mul(number_1, number_2))
+    print(number_1, "*", number_2, "*", number_3, "*", number_4, "=",
+            mul(number_1, number_2, number_3, number_4))
 
 elif select==4:
-    print(number_1, "/", number_2, "=",
-            div(number_1, number_2))
+    print(number_1, "/", number_2, "/", number_3, "/", number_4, "=",
+            div(number_1, number_2, number_3, number_4))
 
 else:
     print("Invalid option")
